89e7cd2e10bd50466fe469e33ac4fae69b67acaa
rar9000 <clavius@Clavius.local>
2024-12-16 16:29:11 -0500

    added fuctionality

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,38 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = 'spawn wings and fly';
+const currentYear = 2025;
+let willMeetResolution = true;
 
 function updateYear() {
     // Update this function
+    yearElement.innerText = currentYear;
 
 }
 
 function updateResolution() {
-    // Update this function 
+    // Update this function
+    resolutionElement.innerText = resolution; 
 
 }
 
 function updateWillMeetResolution() {
     // Update this function
+    willMeetResolutionElement.innerText = willMeetResolution;
 
 }
 
 function render() {
     // Update this function
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
+    render();
 
 })
